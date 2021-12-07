b4553a7c5ff713eed0bdcf6fa682e9f219732122
ogkourlias <o.gkourlias@st.hanze.nl>
2021-12-07 12:04:17 +0100

    Changed f string structure in preprocessing.py so it may apply for all picard programs in a loop

--- lib/preprocessing.py
+++ lib/preprocessing.py
@@ -26,14 +26,32 @@ class Preprocessing:
         self.picard = f"java -jar tools/picard.jar"
 
     def runPicard(self):
-        for alignedFiles in glob.glob(f'{self.outputDir}/Preprocessing/aligned/*.bam'):
+        outputDir = f'{self.outputDir}/Preprocessing'
+        programs = ["SortSam", "AddOrReplaceReadGroups"]
+                    #, "FixMateInformation", "MergeSamFiles", "MarkDuplicates"]
+
+        for alignedFiles in glob.glob(f'{outputDir}/aligned/*.bam'):
             file = re.search(r"[^/]+(?=\.bam)", alignedFiles).group(0)
-            print(f"{self.picard} SortSam I={self.outputDir}/Preprocessing/aligned/{file}.bam O={self.outputDir}"
-                      f"/Preprocessing/sortedBam/{file}.bam SO=queryname")
-            os.system(f"{self.picard} SortSam I={self.outputDir}/Preprocessing/aligned/{file}.bam O={self.outputDir}"
-                      f"/Preprocessing/sortedBam/{file}.bam SO=queryname")
+            options = ["SO=queryname",
+                       f"LB={file} PU={file} SM={file} PL=illumina CREATE_INDEX=true"]
+            for program in programs:
+                os.system(f"{self.picard} {program} "
+                          f"I={alignedFiles} "
+                          f"O={alignedFiles} "
+                          f"{options[programs.index(program)]}")
+            #os.system(f"mv {alignedFiles} {outputDir}/markDuplicates")
 
 
+# os.system(
+#     "java -jar " + picard + " AddOrReplaceReadGroups INPUT=" + outputDir + "/Preprocessing/sortedBam/" + currentFile + ".bam OUTPUT=" + outputDir + "/Preprocessing/addOrReplace/" + currentFile + ".bam " + " LB=" + currentFile + " PU=" + currentFile + " SM=" + currentFile + " PL=illumina CREATE_INDEX=true")
+# os.system(
+#     "java -jar " + picard + " FixMateInformation INPUT=" + outputDir + "/Preprocessing/addOrReplace/" + currentFile + ".bam")
+# os.system(
+#     "java -jar " + picard + " MergeSamFiles INPUT=" + outputDir + "/Preprocessing/addOrReplace/" + currentFile + ".bam OUTPUT=" + outputDir + "/Preprocessing/mergeSam/" + currentFile + ".bam " + " CREATE_INDEX=true USE_THREADING=true")
+# os.system(
+#     "java -jar " + picard + " MarkDuplicates INPUT=" + outputDir + "/Preprocessing/mergeSam/" + currentFile + ".bam OUTPUT=" + outputDir + "/Preprocessing/markDuplicates/" + currentFile + ".bam " + " CREATE_INDEX=true METRICS_FILE=" + outputDir + "/Preprocessing/markDuplicates/" + currentFile + ".metrics.log")
+# os.system(
+#     "samtools sort -n " + outputDir + "/Preprocessing/markDuplicates/" + currentFile + ".bam -o " + outputDir + "/Preprocessing/markDuplicates/" + currentFile + "_sorted.bam")
 
 
 # FUNCTIONS
