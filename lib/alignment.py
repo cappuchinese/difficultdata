f288af6077070c5615396cd56eeb14fd5417cbc2
Kyreria <dennishaandrikman@live.nl>
2021-12-07 12:01:09 +0100

    Updated the alignment.py, old code rewritten and now adding additional paired-end alignment

--- lib/alignment.py
+++ lib/alignment.py
@@ -9,7 +9,7 @@ This script also creates the .bam file for futher use.
 # METADATA VARIABLES
 __author__ = "Dennis Haandrikman"
 __status__ = "Alignment script, WIP"
-__version__ = "0.3"
+__version__ = "0.5"
 
 # Import modules
 import concurrent.futures
@@ -19,13 +19,59 @@ import glob
 
 class Alignment:
 
-    def __init__(self, OutputDir, extension):
-        self.outputdir = OutputDir
+    def __init__(self, outputdir, extension, genomehisat2):
+        # Outputdir is the output directory
+        self.outputdir = outputdir
+        # extension is the file extension, example: .fasta
         self.extension = extension
+        # genomeHiSat2 is the reference genome the alignments will be done
+        self.genome = genomehisat2
 
     def main_process(self):
+        """
+
+        """
         # TODO: Align the files and see about paired end alignments
-        self.uniquefilenames = []
-        files = glob.glob(f"{self.outputdir}/Preprocessing/trimmed/*.{self.extension}")
+        # Load in the names of the files & make sure only unique files are added for alignment
+        self.unique_filenames_unpaired = []
+        self.unique_filenames_paired = []
+        for files in glob.glob(f"{self.outputdir}/Preprocessing/trimmed/*.{self.extension}"):
+            if files not in self.unique_filenames:
+
+                self.unique_filenames.append(files)
+
+
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            alignments = executor.map(self.align, self.unique_filenames)
+
+        for result in alignments:
+            print(result)
+
+    def align(self, file):
+        """
+        This function aligns the sequences in the files to the genome generated beforehand.
+        """
+        filename = file.split("/")
+        fastq_name = filename[0].split(".")
+        os.system(f"hisat2 -x ./{self.genome} -U {filename} 2> "
+                  f"{self.outputdir}/Results/alignment/{fastq_name.replace('_trimmed', '')}.log | samtools view -Sbo "
+                  f"{self.outputdir}/Preprocessing/aligned/{fastq_name.replace('_trimmed', '')}.bam -")
+
+    def paired_align(self, files):
+        """
+        This functions aligns the paired sequences in the files to the genome generated beforehand
+        """
+        file_names = []
+        for file in files:
+            filename = file.split("/")
+            file_names.append(filename)
+            filename = filename[0].split(".")
+            fastq_name = filename[0].split("_")
+
+        os.system(f"hisat2 -x ./{self.genome} -1 {file_names[0]} -2 {file_names[0]} 2> "
+                  f"{self.outputdir}/Results/alignment/{fastq_name}.log | samtools view -Sbo "
+                  f"{self.outputdir}/Preprocessing/aligned/{fastq_name}.bam -")
+
+
 
 
