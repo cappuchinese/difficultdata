3c8ae147cdbf1627cb812acaf42a5711ea495aab
ogkourlias <o.gkourlias@st.hanze.nl>
2021-12-14 15:19:57 +0100

    Refined preprocessing so that it may utilise concurrent.futures

--- lib/preprocessing.py
+++ lib/preprocessing.py
@@ -14,7 +14,6 @@ __version__ = "0.5"
 import sys
 import re
 import os
-import argparse
 import concurrent.futures
 import glob
 
@@ -22,40 +21,41 @@ import glob
 class Preprocessing:
     """ Desc """
     def __init__(self, args):
-        self.outputDir = args.outputDir
+        """"OUTPUT MOET NOG VERANDER WORDEN NAAR ARGS.OUTPUT OFZO"""
+        self.outputDir = "output/Preprocessing"
+        self.files = glob.glob(f'{self.outputDir}/aligned/*.bam')
 
     def runPicard(self):
-        outputDir = f'{self.outputDir}/Preprocessing'
-        os.system(f"mkdir {outputDir}/temp")
+        os.system(f"mkdir {self.outputDir}/temp")
+        executor = concurrent.futures.ProcessPoolExecutor()
+        executor.map(self.processFile, self.files)
+        executor.shutdown()
+        os.system(f"rm -r {self.outputDir}/temp")
 
-        for alignedFiles in glob.glob(f'{outputDir}/aligned/*.bam'):
-            os.system(f"cp {alignedFiles} {outputDir}/temp")
-            file = re.search(r"[^/]+(?=\.bam)", alignedFiles).group(0)
-            alignedFiles = f"{outputDir}/temp/{file}.bam"
-            programs = [("", "SortSam", "SO=queryname", f"java -jar tools/picard.jar"),
+    def processFile(self, file):
+        os.system(f"cp {file} {self.outputDir}/temp")
+        file_name = re.search(r"[^/]+(?=\.bam)", file).group(0)
+        file = f"{self.outputDir}/temp/{file_name}.bam"
+        programs = [("", "SortSam", "SO=queryname", f"java -jar tools/picard.jar"),
 
-                        ("SortSam", "AddOrReplaceReadGroups",
-                         f"LB={file} PU={file} SM={file} PL=illumina CREATE_INDEX=true", f"java -jar tools/picard.jar"),
+                    ("SortSam", "AddOrReplaceReadGroups",
+                     f"LB={file_name} PU={file_name} SM={file_name} PL=illumina CREATE_INDEX=true", f"java -jar tools/picard.jar"),
 
-                        ("AddOrReplaceReadGroups", "FixMateInformation", "", f"java -jar tools/picard.jar"),
+                    ("AddOrReplaceReadGroups", "FixMateInformation", "", f"java -jar tools/picard.jar"),
 
-                        ("FixMateInformation", "MergeSamFiles", "CREATE_INDEX=true USE_THREADING=true",
-                         f"java -jar tools/picard.jar"),
+                    ("FixMateInformation", "MergeSamFiles", "CREATE_INDEX=true USE_THREADING=true",
+                     f"java -jar tools/picard.jar"),
 
-                        ("MergeSamFiles", "MarkDuplicates",
-                         f"CREATE_INDEX=true METRICS_FILE={outputDir}/markDuplicates/{file}.metrics.log",
-                         f"java -jar tools/picard.jar"),
+                    ("MergeSamFiles", "MarkDuplicates",
+                     f"CREATE_INDEX=true METRICS_FILE={self.outputDir}/markDuplicates/{file_name}.metrics.log",
+                     f"java -jar tools/picard.jar"),
 
-                        ("MarkDuplicates", "sort -n", "", "samtools")]
+                    ("MarkDuplicates", "sort -n", "", "samtools")]
 
-            for program in programs:
-                os.system(f"{program[3]} {program[1]} "
-                          f"I={alignedFiles[:-4]}{program[0][0:4]}.bam "
-                          f"O={alignedFiles[:-4]}{program[1][0:4]}.bam "
-                          f"{program[2]}")
+        for program in programs:
+            os.system(f"{program[3]} {program[1]} "
+                      f"I={file[:-4]}{program[0][0:4]}.bam "
+                      f"O={file[:-4]}{program[1][0:4]}.bam "
+                      f"{program[2]}")
 
-            os.system(f"mv {outputDir}/temp/{file}Sort.bam {outputDir}/markDuplicates/{file}_sorted.bam")
-
-        os.system(f"rm -r {outputDir}/temp")
-
-
+        os.system(f"mv {self.outputDir}/temp/{file_name}Sort.bam {self.outputDir}/markDuplicates/{file_name}_sorted.bam")
