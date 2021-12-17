2064841cfbb357a71f93a659874493f0b90c07bd
Kyreria <dennishaandrikman@live.nl>
2021-12-17 15:47:05 +0100

    Checked & updated the documentation of create_count.py, file completely done

--- lib/create_count.py
+++ lib/create_count.py
@@ -1,11 +1,11 @@
 """
-TODO program docstring
+Module to call upon the featureCounts tool
 """
 
 # METADATA VARIABLES
 __author__ = "Mats Slik"
-__status__ = "WIP"
-__version__ = "0.1"
+__status__ = "Finished & checked"
+__version__ = "1.0"
 
 # IMPORTS
 import subprocess
@@ -14,13 +14,14 @@ from termcolor import colored
 
 class FeatureCount:
     """
-    class feature count: directs multiple paths to run the tool featureCount and creat a output file
+    class feature count:    directs multiple paths to run the tool featureCount
+                            and create a .bam output file & a geneCounts .txt file
     """
     def __init__(self, outputdir, gtffile):
         """
-        initialization
-        :param: outputdir: path to the output directory
-        :param: gtffile: genome annotation file path
+        initialization of the class
+        :param: outputdir:  a string, path to the output directory
+        :param: gtffile:    a string, genome annotation file path
         """
         self.outputdir = outputdir
         self.feature_count = f"tools/Subread-2.0.0/bin/featureCounts"
@@ -28,8 +29,9 @@ class FeatureCount:
 
     def write_file(self):
         """
-        perform featureCounts
-        :param: gtffile: genome annotation file path
+        perform the script used to run the featureCounts tool
+        :param: gtffile:    a string, genome annotation file path
+        :OUTPUT:            2 files, a .txt with gene counts and a .bam file with marked duplicates
         """
         print(colored("Using featureCount...", "blue", attrs=["bold"]))
         subprocess.run([self.feature_count, "-a", {self.gtffile},
